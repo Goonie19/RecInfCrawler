84f1341c458cb679777cc9145fcec91095a33b5d
Goonie19 <strawhatgoonie@gmail.com>
2019-11-13 17:46:09 +0100

    Solo imprime URLs con su html en español

--- src/main/java/Crawler/app/RecInfCrawler.java
+++ src/main/java/Crawler/app/RecInfCrawler.java
@@ -17,12 +17,12 @@ public class RecInfCrawler {
         links = new HashSet<String>();
     }
 
-    public void getPageLinks(String URL) {
+    public void getPages(String URL) {
         //4. Check if you have already crawled the URLs 
         //(we are intentionally not checking for duplicate content in this example)
         if (!links.contains(URL)) {
             try {
-                //4. (i) If not add it to the index
+
                 if (links.add(URL)) {
                     System.out.println(URL);
                 }
@@ -30,12 +30,14 @@ public class RecInfCrawler {
                 //2. Fetch the HTML code
                 Document document = Jsoup.connect(URL).get();
                 //3. Parse the HTML to extract links to other URLs
-                Elements linksOnPage = document.select("a[href]");
+                Elements elementos = document.select("a[href]");
+                Element lenguaje = document.select("html[lang]").first();
 
-                //5. For each extracted URL... go back to Step 4.
-                for (Element page : linksOnPage) {
-                    getPageLinks(page.attr("abs:href"));
+                for(Element el : elementos) {
+                    if(lenguaje.attributes().get("lang").equals("es"))
+                        getPages(el.attr("abs:href"));
                 }
+
             } catch (IOException e) {
                 System.err.println("For '" + URL + "': " + e.getMessage());
             }
@@ -44,7 +46,7 @@ public class RecInfCrawler {
 
     public static void main(String[] args) {
         //1. Pick a URL from the frontier
-        new RecInfCrawler().getPageLinks("https://es.wikipedia.org/wiki/Wikipedia:Portada");
+        new RecInfCrawler().getPages("https://es.wikipedia.org/wiki/Wikipedia:Portada");
     }
 
 }
