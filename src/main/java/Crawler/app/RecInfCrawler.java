f5ce9d5eafde2f15b9fac262e27b56efb4534821
Goonie19 <strawhatgoonie@gmail.com>
2019-11-26 17:07:17 +0100

    Version funcional del Crawler en anchura

--- src/main/java/Crawler/app/RecInfCrawler.java
+++ src/main/java/Crawler/app/RecInfCrawler.java
@@ -10,48 +10,50 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class RecInfCrawler {
 
-    private ArrayList<String> links;
-    private HashSet<String> linksVisitados = new HashSet<String>();
+    private final Queue<String> links;
+    private final HashSet<String> linksVisitados = new HashSet<String>();
 
     public RecInfCrawler() {
-        links = new ArrayList<String>();
+        links = new LinkedList<>();
     }
 
-    public void getPages(String URL) throws Exception {
+    public void getPages(final String URL) throws Exception {
         links.add(URL);
-        int i = 0;
        try {
         while(true) {
 
-            Document document = Jsoup.connect(links.get(i)).get();
-            Elements elementos = document.select("a[href]");
-            Element lenguaje = document.select("html[lang]").first();
-            Element titulo = document.select("title").first();
+            final Document document = Jsoup.connect(links.peek()).get();
+            final Elements elementos = document.select("a[href]");
+            final Element lenguaje = document.select("html[lang]").first();
+            final Element titulo = document.select("title").first();
 
-            System.out.println(links.get(links.size() -1));
-            for (Element el : elementos) {
-
-                links.add(links.size() -1 , el.attr("abs:href"));
+            System.out.println(links.peek());
+            for (final Element el : elementos) {
+                if(lenguaje.attributes().get("lang").equals("es") && !el.attributes().get("href").contains("?") &&
+                !el.attributes().get("href").contains(":") && !el.attributes().get("href").contains("#") &&
+                 el.attributes().get("href").contains("wiki") && !linksVisitados.contains(el.attr("abs:href"))) {
+                    links.add(el.attr("abs:href"));
+                }
             }
                
-            if(lenguaje.attributes().get("lang").equals("es") && !links.get(i).contains("?") 
-            && !links.get(i).contains(":") && !links.get(i).contains("#") && links.get(i).contains("wiki")) {
-                File f = new File("./././HTML/" + titulo.text() + ".html");
-                FileWriter writer = new FileWriter(f);
-                writer.write(document.outerHtml());
-                writer.close();
-            }
-            ++i;
+            final File f = new File("./././HTML/" + titulo.text() + ".html");
+            final FileWriter writer = new FileWriter(f);
+            writer.write(document.outerHtml());
+            writer.close();
+            linksVisitados.add(links.poll());
+
         }
-       } catch(IOException e) {
+       } catch(final IOException e) {
            e.printStackTrace();
        }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(final String[] args) throws Exception {
         //1. Pick a URL from the frontier
         new RecInfCrawler().getPages("https://es.wikipedia.org/wiki/Wikipedia:Portada");
     }
